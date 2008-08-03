3697644ae447f08bdfc175b8086db4d07db74cde
Aleksey Tulinov <aleksey_t@users.sourceforge.net>
2008-08-03 20:35:09 +0000

    +added OS_SPECIFIC_CFLAGS support to makefiles
    git-svn-id: https://svn.code.sf.net/p/remotefs/code/trunk@84 7764f161-3e89-4957-9ae6-8e1f4830ae4a

--- Makefiles/base.mk
+++ Makefiles/base.mk
@@ -1,15 +1,23 @@
 
+# gmake
+OS=$(shell uname)
+# Solaris, FreeBSD
+OS:sh=uname
+
+include Makefiles/$(OS).mk
+
+CFLAGS += $(OS_SPECIFIC_CFLAGS)
 CFLAGS += -Wall -Werror
 
 all: build
 build: $(OBJS) link
 
 %.o : %.c
-	@echo "Compiling $<"
+	@echo "Compiling $< [$(CFLAGS)]"
 	@$(CC) -c $(CFLAGS) $< -o $@
 	
 link:
-	@echo "Linking $(TARGET)"
+	@echo "Linking $(TARGET) [$(LDFLAGS)]"
 	@$(CC) $(OBJS) $(LDFLAGS) -o $(TARGET)
 
 clean:
