7bafe6ae12c1b66d39350845b994055f9c19bb7b
Aleksey Tulinov <aleksey_t@users.sourceforge.net>
2008-08-10 13:09:17 +0000

    +removed fancy memory allocation
    git-svn-id: https://svn.code.sf.net/p/remotefs/code/trunk@88 7764f161-3e89-4957-9ae6-8e1f4830ae4a

--- src/alloc.c
+++ src/alloc.c
@@ -2,55 +2,16 @@
 
 #include <stdlib.h>
 
-#include "config.h"
-
-static char *last_allocated = NULL;
-static size_t last_allocated_size = 0;
-static int freed = 0;
-
 void *mp_alloc(size_t size)
 {
-	if (freed != 0
-	&& last_allocated != NULL)
-	{
-		if (size == last_allocated_size)
-		{
-			freed = 0;
-			return last_allocated;
-		}
-		else
-		{
-			free(last_allocated);
-		}
-	}
-	
-	last_allocated = malloc(size);
-	last_allocated_size = (last_allocated != NULL ? size : 0);
-	freed = 0;
-	
-	return last_allocated;
+	return malloc(size);
 }
 
 void mp_free(void *buffer)
 {
-	if (buffer == last_allocated)
-	{
-		freed = 1;
-	}
-	else
-	{
-		free(buffer);
-	}
+	return free(buffer);
 }
 
 void mp_force_free()
 {
-	if (last_allocated != NULL
-	&& freed != 0)
-	{
-		free(last_allocated);
-		last_allocated = NULL;
-		last_allocated_size = 0;
-		freed = 1;
-	}
 }
