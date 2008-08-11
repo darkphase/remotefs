3bf7436b3a579010ddae27b94d3b9b12c28edc9f
Jean-Jacques Sarton <jjsarton@users.sourceforge.net>
2008-08-11 12:59:49 +0000

    added rule for rpm
    git-svn-id: https://svn.code.sf.net/p/remotefs/code/trunk@155 7764f161-3e89-4957-9ae6-8e1f4830ae4a

--- Makefiles/base.mk
+++ Makefiles/base.mk
@@ -28,6 +28,37 @@ flags:
 	@echo LDFLAGS = $(LDFLAGS)
 	@echo
 
+bldrpm:
+	@( \
+	if [ "$(MACHINE)" = "" ]; then MAC=`uname -m`; else NAC=$(MACHINE); fi ; \
+	RPMD=`rpm -q --eval=%{_topdir} --specfile $(RPMNAME).spec | grep -v $(RPMNAME)`; \
+	if [ -d "$$RPMD" -a -w "$$RPMD" ];\
+	then \
+	   VER=`sed -n 's/Version:\(.*\)/\1/p' rfsd.spec|tr -d ' '`; \
+	   SDIR=`pwd`; \
+	   NM=$(RPMNAME)-$$VER; \
+	   mkdir /tmp/$$NM ;\
+	   cp -r * /tmp/$$NM; \
+	   find /tmp/$$NM -name .svn -exec rm -fr {} \; ; \
+	   cd /tmp ; \
+	   tar czf $$RPMD/SOURCES/$$NM.tar.gz $$NM ; \
+	   cd $$SDIR ; \
+	   if [ -x /usr/bin/rpmbuild ]; \
+	   then \
+	      rpmbuild -ba --target $$MAC /tmp/$$NM/$(RPMNAME).spec; \
+	   else \
+	      rpm -ba $$NM/$(RPMNAME).spec; \
+	   fi; \
+	   rm -fr /tmp/$$NM 2>/dev/tty 1>/dev/tty; \
+	else \
+	   echo You must be root for this.; \
+	   echo Vous devez etre root pour ceci.; \
+	   echo Sie muessen root sein.; \
+	   echo Devi essere root per fare questo.; \
+	   exit 2; \
+	fi;\
+	)
+
 #############################
 # Dependencies for all proj.
 #############################
