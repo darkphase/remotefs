ab384ae68d9201c51cd9d2950068e0176661919c
Jean-Jacques Sarton <jjsarton@users.sourceforge.net>
2008-08-12 10:23:02 +0000

    first version for FreeBSD specific Makefile
    git-svn-id: https://svn.code.sf.net/p/remotefs/code/trunk@171 7764f161-3e89-4957-9ae6-8e1f4830ae4a

---
+++ Makefiles/FreeBSD.mk
@@ -0,0 +1,31 @@
+################################
+# The executables
+################################
+
+MAKE = make
+CC = cc
+AR = ar
+RM = rm
+
+################################
+# OS / CC specifics flags
+################################
+
+CFLAGS_O     = -I./src -DFREEBSD -DEBADE=EINVAL -DEREMOTEIO=ECANCELED -DNAME_MAX=255 
+CFLAGS_DBG   = -g
+CFLAGS_OPT   = -O3
+
+###############################
+# Flags needed for Fuse
+###############################
+
+CFLAGS_FUSE  = -I/usr/include/fuse -D_FILE_OFFSET_BITS=64 -DFUSE_USE_VERSION=25
+LDFLAGS_FUSE = -lfuse
+
+###############################
+# Flags for linking
+###############################
+
+LDFLAGS_NET   =
+#LDFLAGS_PTHR  = -lpthread
+LDFLAGS_CRYPT = -lcrypt
