df5e6827923c1fd1bbf1d7414d451020bb9b7292
Aleksey Tulinov <aleksey_t@users.sourceforge.net>
2008-08-17 16:32:14 +0000

    +path_join() fixed
    git-svn-id: https://svn.code.sf.net/p/remotefs/code/trunk@194 7764f161-3e89-4957-9ae6-8e1f4830ae4a

--- src/path.c
+++ src/path.c
@@ -1,11 +1,15 @@
 #include "path.h"
 
 #include <string.h>
+#include "config.h"
 
 int path_join(char full_path[NAME_MAX], const char *path, const char *filename)
 {
 	unsigned path_len = strlen(path);
 	unsigned filename_len = strlen(filename);
+	
+	DEBUG("%s %d\n", path, path_len);
+	DEBUG("%s %d\n", filename, filename_len);
 	
 	if (path_len + filename_len + 1 > NAME_MAX)
 	{
@@ -13,25 +17,26 @@ int path_join(char full_path[NAME_MAX], const char *path, const char *filename)
 	}
 
 	memset(full_path, 0, NAME_MAX);
-		
+	
 	const unsigned char copy_path = 
 	(strcmp(filename, ".") == 0 || strcmp(filename, "..") == 0 )
 	? 0
 	: 1;
-	const unsigned char add_slash = (copy_path == 1 && path[path_len - 2] == '/' ? 0 : 1);
+	const unsigned char add_slash = ((copy_path == 1 && path[path_len - 1] == '/') ? 0 : 1);
 	
 	if (copy_path == 1)
 	{
-		memcpy(full_path, path, path_len - 1);
+		memcpy(full_path, path, path_len);
 	}
 	
 	if (copy_path == 1
 	&& add_slash == 1)
 	{
-		memcpy(full_path + path_len - 1, "/", 1);
+		memcpy(full_path + path_len, "/", 1);
 	}
 	
-	memcpy(full_path + (copy_path == 1 ? path_len - 1 + add_slash : 0), 
+	memcpy(full_path + (copy_path == 1 ? path_len + add_slash : 0), 
 	filename, 
 	strlen(filename));
-		return 0;}
+	
+	return 0;}
