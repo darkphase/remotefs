089f81861f862f0c72e245412827369f8c1f4987
Aleksey Tulinov <aleksey_t@users.sourceforge.net>
2009-01-30 20:10:45 +0000

    +makefile for ARMEB cross-cc
    git-svn-id: https://svn.code.sf.net/p/remotefs/code/trunk@652 7764f161-3e89-4957-9ae6-8e1f4830ae4a

---
+++ Makefiles/LinuxARMEB.mk
@@ -0,0 +1,49 @@
+################################
+# The executables
+################################
+
+TOOLCHAIN_ROOT = toolchains/toolchain-armeb_gcc4.1.2
+ARCH = armeb
+MAKE = make
+CC = "$(TOOLCHAIN_ROOT)/bin/armeb-linux-uclibc-gcc"
+AR = "$(TOOLCHAIN_ROOT)/bin/armeb-linux-uclibc-ar"
+RM = rm
+LN = ln -sf
+
+################################
+# OS / CC specifics flags
+################################
+
+CFLAGS_OS      = -Wall -Werror
+CFLAGS_DEBUG   = -g
+CFLAGS_RELEASE = -O2
+
+###############################
+# Flags needed for Fuse
+###############################
+
+CFLAGS_FUSE  = `pkg-config --cflags fuse` -DFUSE_USE_VERSION=26
+LDFLAGS_FUSE = `pkg-config --libs fuse`
+
+###############################
+# Flags for linking
+###############################
+
+LDFLAGS_DEBUG   = -g
+LDFLAGS_RELEASE = -s -O2
+LDFLAGS_SSL     = -lssl
+
+###############################
+# Flags for dymamic libraries
+###############################
+
+CFLAGS_SO    = -fPIC
+LDFLAGS_SO   = -shared -Wl,-soname,$(SO_NAME)
+SO_EXT       = so
+SO_NAME      = $(TARGET).$(SO_EXT).$(VERSION)
+
+###############################
+# Optional OS dependent program
+###############################
+
+RFS_NSS = rfs_nss
