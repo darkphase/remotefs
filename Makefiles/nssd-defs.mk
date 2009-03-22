c5803ce00352d93351de92435742d67f75783cf6
Aleksey Tulinov <aleksey_t@users.sourceforge.net>
2009-03-22 15:23:26 +0000

    +rfs_nss integration to builds
    git-svn-id: https://svn.code.sf.net/p/remotefs/code/trunk@746 7764f161-3e89-4957-9ae6-8e1f4830ae4a

--- Makefiles/nssd-defs.mk
+++ Makefiles/nssd-defs.mk
@@ -4,17 +4,20 @@
 # flags passed by the caller (?FLAGS_G)
 #######################################
 
-nssd_TARGET = rfs_nss
+nssd_TARGET = rfs_nssd
+rfs_INCLUDES= -Isrc/
 
 nssd_CFLAGS  =  $(CFLAGS_MAIN) \
                 $(CFLAGS_OS) \
                 $(CFLAGS_OPTS) \
-                $(OPTS)
+                $(OPTS) \
+				$(rfs_INCLUDES)
 
 nssd_LDFLAGS = $(LDFLAGS_MAIN) \
                $(LDFLAGS_OS) \
                $(LDFLAGS_NET) \
                $(LDFLAGS_OPTS) \
+			   -L. -lrfs
 
 #######################################
 # Define target and object files
@@ -23,5 +26,5 @@ nssd_LDFLAGS = $(LDFLAGS_MAIN) \
 nssd_OBJS = rfs_nss/src/dllist.o \
             rfs_nss/src/rfs_nss_server.o \
             rfs_nss/src/rfs_nss_control.o \
-            rfs_nss/src/rfs_getnames.o
+            rfs_nss/src/rfs_getnames.o \
 
