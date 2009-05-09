d518ebe3bcd0ea32e7990b9926dd9d6f277f4b6a
Aleksey Tulinov <aleksey_t@users.sourceforge.net>
2009-05-09 15:46:43 +0000

    +buffer functions are inlined
    git-svn-id: https://svn.code.sf.net/p/remotefs/code/trunk@831 7764f161-3e89-4957-9ae6-8e1f4830ae4a

--- src/buffer.c
+++ src/buffer.c
@@ -7,92 +7,19 @@ See the file LICENSE.
 */
 
 #include <ctype.h>
-#include <stdlib.h>
-#include <string.h>
 
 #include "buffer.h"
 #include "config.h"
-#include "inet.h"
 
-void *get_buffer(const size_t size)
-{
-	return malloc(size);
-}
-
-void free_buffer(void *buffer)
-{
-	free(buffer);
-}
-
-off_t pack(const void *data, const size_t size, char *buffer, const off_t offset)
-{
-	memcpy(buffer + offset, data, size);
-	return offset + size;
-}
-
-off_t pack_16(const uint16_t *data, char *buffer, const off_t offset)
-{
-	*((uint16_t *)(buffer + offset)) = htons(*data);
-	return offset + sizeof(*data);
-}
-
-off_t pack_32(const uint32_t *data, char *buffer, const off_t offset)
-{
-	*((uint32_t *)(buffer + offset)) = htonl(*data);
-	return offset + sizeof(*data);
-}
-
-off_t pack_32_s(const int32_t *data, char *buffer, const off_t offset)
-{
-	*((int32_t *)(buffer + offset)) = htonl(*data);
-	return offset + sizeof(*data);
-}
-
-off_t pack_64(const uint64_t *data, char *buffer, const off_t offset)
-{
-	*((uint64_t *)(buffer + offset)) = htonll(*data);
-	return offset + sizeof(*data);
-}
-
-off_t unpack(void *data, const size_t size, const char *buffer, const off_t offset)
-{
-	memcpy(data, buffer + offset, size);
-	return offset + size;
-}
-
-off_t unpack_16(uint16_t *data, const char *buffer, const off_t offset)
-{
-	*data = ntohs(*((uint16_t *)(buffer + offset)));
-	return offset + sizeof(*data);
-}
-
-off_t unpack_32(uint32_t *data, const char *buffer, const off_t offset)
-{
-	*data = ntohl(*((uint32_t *)(buffer + offset)));
-	return offset + sizeof(*data);
-}
-
-off_t unpack_32_s(int32_t *data, const char *buffer, const off_t offset)
-{
-	*data = ntohl(*((int32_t *)(buffer + offset)));
-	return offset + sizeof(*data);
-}
-
-off_t unpack_64(uint64_t *data, const char *buffer, const off_t offset)
-{
-	*data = ntohll(*((uint64_t *)(buffer + offset)));
-	return offset + sizeof(*data);
-}
-
+#ifdef RFS_DEBUG
 void dump(const void *data, const size_t data_len)
 {
-#ifdef RFS_DEBUG
 	DEBUG("dumping %u bytes:\n", (unsigned int)data_len);
 	int i = 0; for (i = 0; i < data_len; ++i)
 	{
 		DEBUG("%c", isgraph(((const char *)data)[i]) ? ((const char *)data)[i] : '.');
 	}
 	DEBUG("%s", "\n");
-#endif /* RFS_DEBUG */
 }
+#endif /* RFS_DEBUG */
 
--- src/buffer.h
+++ src/buffer.h
@@ -12,62 +12,116 @@ See the file LICENSE.
 /** memory allocation routines */
 
 #include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
 #include <sys/types.h>
+
+#include "inet.h"
 
 #if defined (__cplusplus) || defined (c_plusplus)
 extern "C" {
 #endif
 
 /** allocate buffer of specified size */
-void* get_buffer(const size_t size);
+static inline void* get_buffer(const size_t size)
+{
+	return malloc(size);
+}
 
 /** free buffer */
-void free_buffer(void *buffer);
+static inline void free_buffer(void *buffer)
+{
+	free(buffer);
+}
 
-/** append (copy) data to buffer */
-off_t pack(const void *data, const size_t size, char *buffer, const off_t offset);
+/** copy data to buffer */
+static inline off_t pack(const void *data, const size_t size, char *buffer, const off_t offset)
+{
+	memcpy(buffer + offset, data, size);
+	return offset + size;
+}
 
-/** append (copy) uint16_t to buffer 
+/** copy uint16_t to buffer 
 @return offset after appended data
 */
-off_t pack_16(const uint16_t *data, char *buffer, const off_t offset);
+static inline off_t pack_16(const uint16_t *data, char *buffer, const off_t offset)
+{
+	*((uint16_t *)(buffer + offset)) = htons(*data);
+	return offset + sizeof(*data);
+}
 
-/** append (copy) uint32_t to buffer 
+/** copy uint32_t to buffer 
 @return offset after appended data
 */
-off_t pack_32(const uint32_t *data, char *buffer, const off_t offset);
+static inline off_t pack_32(const uint32_t *data, char *buffer, const off_t offset)
+{
+	*((uint32_t *)(buffer + offset)) = htonl(*data);
+	return offset + sizeof(*data);
+}
 
-/** same as pack_32, but signed */
-off_t pack_32_s(const int32_t *data, char *buffer, const off_t offset);
+/** same as pack_32, but for signed int */
+static inline off_t pack_32_s(const int32_t *data, char *buffer, const off_t offset)
+{
+	*((int32_t *)(buffer + offset)) = htonl(*data);
+	return offset + sizeof(*data);
+}
 
-/** append (copy) uint64_t to buffer 
+/** copy uint64_t to buffer 
 @return offset after appended data
 */
-off_t pack_64(const uint64_t *data, char *buffer, const off_t offset);
+static inline off_t pack_64(const uint64_t *data, char *buffer, const off_t offset)
+{
+	*((uint64_t *)(buffer + offset)) = htonll(*data);
+	return offset + sizeof(*data);
+}
 
-/** get (copy) data from buffer 
+/** copy data from buffer 
 @return offset after copied data
 */
-off_t unpack(void *data, const size_t size, const char *buffer, const off_t offset);
+static inline off_t unpack(void *data, const size_t size, const char *buffer, const off_t offset)
+{
+	memcpy(data, buffer + offset, size);
+	return offset + size;
+}
 
-/** get (copy) uint16_t from buffer 
+/** copy uint16_t from buffer 
 @return offset after copied data
 */
-off_t unpack_16(uint16_t *data, const char *buffer, const off_t offset);
+static inline off_t unpack_16(uint16_t *data, const char *buffer, const off_t offset)
+{
+	*data = ntohs(*((uint16_t *)(buffer + offset)));
+	return offset + sizeof(*data);
+}
 
-/** get (copy) uint32_t from buffer 
+/** copy uint32_t from buffer 
 @return offset after copied data
 */
-off_t unpack_32(uint32_t *data, const char *buffer, const off_t offset);
-off_t unpack_32_s(int32_t *data, const char *buffer, const off_t offset);
+static inline off_t unpack_32(uint32_t *data, const char *buffer, const off_t offset)
+{
+	*data = ntohl(*((uint32_t *)(buffer + offset)));
+	return offset + sizeof(*data);
+}
 
-/** get (copy) uint64_t from buffer 
+/** same as unpack_32, but for signed int */
+static inline off_t unpack_32_s(int32_t *data, const char *buffer, const off_t offset)
+{
+	*data = ntohl(*((int32_t *)(buffer + offset)));
+	return offset + sizeof(*data);
+}
+
+/** copy uint64_t from buffer 
 @return offset after copied data
 */
-off_t unpack_64(uint64_t *data, const char *buffer, const off_t offset);
+static inline off_t unpack_64(uint64_t *data, const char *buffer, const off_t offset)
+{
+	*data = ntohll(*((uint64_t *)(buffer + offset)));
+	return offset + sizeof(*data);
+}
 
+#ifdef RFS_DEBUG
 /** print buffer to output. debug only */
 void dump(const void *data, const size_t data_len);
+#endif
 
 #if defined (__cplusplus) || defined (c_plusplus)
 }
