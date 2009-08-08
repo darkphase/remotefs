d84db180448d16d44bada83047135f1298fc98a8
Aleksey Tulinov <aleksey_t@users.sourceforge.net>
2009-08-08 06:29:18 +0000

    +cleanup
    git-svn-id: https://svn.code.sf.net/p/remotefs/code/trunk@957 7764f161-3e89-4957-9ae6-8e1f4830ae4a

---
+++ src/options.h
@@ -0,0 +1,23 @@
+/*
+remotefs file system
+See the file AUTHORS for copyright information.
+	
+This program can be distributed under the terms of the GNU GPL.
+See the file LICENSE.
+*/
+
+#ifndef OPTIONS_H
+#define OPTIONS_H
+
+/** options availability defines */
+
+#if (defined LINUX || defined SOLARIS || defined FREEBSD) && defined WITH_UGO
+#	define RFSNSS_AVAILABLE
+#endif
+
+#if (defined LINUX || defined SOLARIS || defined FREEBSD || defined DARWIN)
+#	define SENDFILE_AVAILABLE
+#endif
+
+#endif /* OPTIONS_H */
+
