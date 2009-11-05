9b730d096de10313b23c1e05a5cb5f5a7d0f28ae
Aleksey Tulinov <aleksey_t@users.sourceforge.net>
2009-11-05 22:05:45 +0000

    +removed custom.mk from packaging +fixed tbz packaging
    git-svn-id: https://svn.code.sf.net/p/remotefs/code/trunk@1061 7764f161-3e89-4957-9ae6-8e1f4830ae4a

--- build/Makefiles/packaging/tbz.mk
+++ build/Makefiles/packaging/tbz.mk
@@ -1,13 +1,16 @@
 
+TEMP_TBZ = "remotefs.tar.bz2"
+
 #############################
 # remove all temporary objects
 #############################
 
 clean_tbz_tmp: dummy
 	$(RM) -fr remotefs-*/
+	$(RM) -f "$(TEMP_TBZ)"
 
 clean_tbz: dummy clean_tbz_tmp
-	$(RM) -f remotefs-${VERSION}-${RELEASE}.tar.bz2
+	$(RM) -f remotefs-*.tar.bz2
 
 #############################
 # Build tarball
@@ -16,10 +19,15 @@ clean_tbz: dummy clean_tbz_tmp
 tbz: 	
 	$(MAKE) -sf build/Makefiles/base.mk clean_tmp
 	echo "Building remotefs-$(VERSION)-$(RELEASE).tar.bz2"
+	
 	chmod 700 build/init.d/rfsd.*
 	
-	tar --exclude .svn -cjf "remotefs-$(VERSION)-$(RELEASE).tar.bz2" \
-	src rfs_nss build Makefile custom.mk \
+	tar --exclude .svn -cjf "$(TEMP_TBZ)" \
+	src rfs_nss build Makefile \
 	README LICENSE AUTHORS CHANGELOG
+	
+	mkdir -p "remotefs-$(VERSION)-$(RELEASE)"/
+	tar -xjf "$(TEMP_TBZ)" -C "remotefs-$(VERSION)-$(RELEASE)"/
+	tar -cjf "remotefs-$(VERSION)-$(RELEASE).tar.bz2" "remotefs-$(VERSION)-$(RELEASE)"/
 	
 	$(MAKE) -sf build/Makefiles/base.mk clean_tmp
