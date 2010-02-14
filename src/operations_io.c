da17023d9ff756bdaa35f4038da152ff292789a8
Aleksey Tulinov <aleksey_t@users.sourceforge.net>
2010-02-14 10:27:28 +0000

    +rfs_open() will return stat for FUSE to use it from cache
    git-svn-id: https://svn.code.sf.net/p/remotefs/code/trunk@1135 7764f161-3e89-4957-9ae6-8e1f4830ae4a

--- src/operations_io.c
+++ src/operations_io.c
@@ -68,36 +68,67 @@ int _rfs_open(struct rfs_instance *instance, const char *path, int flags, uint64
 		return cleanup_badmsg(instance, &ans);
 	}
 
-	if (ans.ret != -1)
-	{
-		uint64_t handle = (uint64_t)-1;
-		
-		if (ans.data_len != sizeof(handle))
-		{
-			return cleanup_badmsg(instance, &ans);
-		}
-		
-		if (rfs_receive_data(&instance->sendrecv, &handle, ans.data_len) == -1)
-		{
-			return -ECONNABORTED;
-		}
-		
-		*desc = ntohll(handle);
-	}
-
 	if (ans.ret == -1)
 	{
 		if (ans.ret_errno == -ENOENT)
 		{
 			delete_from_cache(&instance->attr_cache, path);
 		}
+
+		return -ans.ret_errno;
+	}
+
+	uint32_t stat_failed = 0;
+	uint32_t user_len = 0;
+	uint32_t group_len = 0;
+
+#define ans_buffer_size sizeof(*desc) \
++ sizeof(stat_failed) + STAT_BLOCK_SIZE + sizeof(user_len) + sizeof(group_len) \
++ (MAX_SUPPORTED_NAME_LEN + 1) + (MAX_SUPPORTED_NAME_LEN + 1)
+
+	char ans_buffer[ans_buffer_size]  = { 0 };
+	
+	if (ans.data_len > sizeof(ans_buffer))
+	{
+		return cleanup_badmsg(instance, &ans);
+	}
+#undef ans_buffer_size
+	
+	if (rfs_receive_data(&instance->sendrecv, ans_buffer, ans.data_len) == -1)
+	{
+		return -ECONNABORTED;
+	}
+
+	struct stat stbuf = { 0 };
+
+	const char *user = 
+	unpack_32(&group_len, 
+	unpack_32(&user_len, 
+	unpack_stat(&stbuf, 
+	unpack_32(&stat_failed, 
+	unpack_64(desc, ans_buffer
+	)))));
+	const char *group = user + user_len;
+
+	DEBUG("handle: %llu\n", (long long unsigned)(*desc));
+
+	stbuf.st_uid = resolve_username(instance, user);
+	stbuf.st_gid = resolve_groupname(instance, group, user);
+
+	if (ans.ret_errno == 0)
+	{
+		if (stat_failed == 0)
+		{
+			cache_file(&instance->attr_cache, path, &stbuf);
+		}
+		
+		resume_add_file_to_open_list(&instance->resume.open_files, path, flags, *desc);
 	}
 	else
 	{
 		delete_from_cache(&instance->attr_cache, path);
-		resume_add_file_to_open_list(&instance->resume.open_files, path, flags, *desc);
 	}
-
+	
 	return ans.ret == -1 ? -ans.ret_errno : ans.ret;
 }
 
--- src/server_handlers_io.c
+++ src/server_handlers_io.c
@@ -15,6 +15,8 @@ See the file LICENSE.
 #include "buffer.h"
 #include "command.h"
 #include "config.h"
+#include "exports.h"
+#include "id_lookup.h"
 #include "instance_server.h"
 #include "resume/cleanup.h"
 #include "sendrecv_server.h"
@@ -47,13 +49,10 @@ int _handle_open(struct rfsd_instance *instance, const struct sockaddr_in *clien
 	}
 	
 	int flags = os_file_flags(rfs_flags);
-	uint64_t handle = (uint64_t)(-1);
 	
 	errno = 0;
 	int fd = open(path, flags);
-	
-	struct answer ans = { cmd_open, (fd == -1 ? 0 : sizeof(handle)), (fd == -1 ? -1 : 0), errno };
-	
+		
 	free(buffer);
 	
 	if (fd != -1)
@@ -65,27 +64,67 @@ int _handle_open(struct rfsd_instance *instance, const struct sockaddr_in *clien
 		}
 	}
 
-	if (ans.ret == -1)
+	if (fd == -1)
 	{
+		struct answer ans = { cmd_open, 0, -1, errno };
 		if (rfs_send_answer(&instance->sendrecv, &ans) == -1)
 		{
 			return -1;
 		}
-	}
-	else
-	{
-		handle = htonll((uint64_t)fd);
-		
-		send_token_t token = { 0, {{ 0 }} };
-		if (do_send(&instance->sendrecv, 
-			queue_data((void *)&handle, sizeof(handle), 
-			queue_ans(&ans, &token))) < 0)
-		{
-			return -1;
-		}
+
+		return 1;
 	}
 
-	return 0;
+	uint64_t handle = (uint64_t)(fd);
+
+	uint32_t stat_failed = 0;
+	struct stat stbuf = { 0 };
+
+	stat_failed = (fstat(fd, &stbuf) == 0 ? 0 : 1);
+
+	uint32_t user_len = 0, user_len_hton = 0;
+	uint32_t group_len = 0, group_len_hton = 0;
+	char stat_buffer[STAT_BLOCK_SIZE] = { 0 };
+	const char *user = NULL, *group = NULL;
+
+#ifdef WITH_UGO
+	if (stat_failed == 0 
+	&& (instance->server.mounted_export->options & OPT_UGO) != 0)
+	{
+		user = get_uid_name(instance->id_lookup.uids, stbuf.st_uid);
+		group = get_gid_name(instance->id_lookup.gids, stbuf.st_gid);
+	}
+#endif
+
+	if (user == NULL) { user = ""; }
+	if (group == NULL) { group = ""; }
+	
+	user_len = user_len_hton = strlen(user) + 1;
+	group_len = group_len_hton = strlen(group) + 1;
+	
+	unsigned overall_size = sizeof(handle) + sizeof(stat_failed) + STAT_BLOCK_SIZE + sizeof(user_len) + sizeof(group_len) + user_len + group_len;
+
+	struct answer ans = { cmd_open, overall_size, 0, 0 };
+
+	DEBUG("mode: %u, size: %lu\n", (unsigned)stbuf.st_mode, (long unsigned)stbuf.st_size);
+
+	pack_stat(&stbuf, stat_buffer);
+
+	DEBUG("user: %s, user_len: %lu, group: %s, group_len: %lu\n", 
+		user, (unsigned long)user_len, 
+		group, (unsigned long)group_len);
+	
+	send_token_t token = { 0, {{ 0 }} };
+	return (do_send(&instance->sendrecv, 
+		queue_data(group, group_len, 
+		queue_data(user, user_len, 
+		queue_32(&group_len_hton, 
+		queue_32(&user_len_hton, 
+		queue_data((const char *)stat_buffer, sizeof(stat_buffer), 
+		queue_32(&stat_failed, 
+		queue_64(&handle, 
+		queue_ans(&ans, &token 
+		))))))))) < 0) ? -1 : 0;
 }
 
 int _handle_release(struct rfsd_instance *instance, const struct sockaddr_in *client_addr, const struct command *cmd)
