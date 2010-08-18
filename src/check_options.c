4cd01ae9b06e38981c286c6b0f9f5184388b6825
Aleksey Tulinov <aleksey_t@users.sourceforge.net>
2010-08-18 08:11:58 +0000

    +clearer compile error message
    git-svn-id: https://remotefs.svn.sourceforge.net/svnroot/remotefs/rfs_nss/trunk@1236 a487bacb-bc4e-0410-a857-d1bad4672f5a

--- src/check_options.c
+++ src/check_options.c
@@ -11,7 +11,7 @@ See the file LICENSE.
 /* this file should be compiled first for rfs_nssd and libnss_rfs */
 
 #ifndef RFSNSS_AVAILABLE
-#	error "Sorry, but rfs_nss isn't supported for this OS or with current build options"
+#	error "Sorry, but rfs_nss isn't supported for this OS or with current build options (you need at least WITH_UGO enabled in options.mk)"
 #else
 int check_options_c_dummy = 0;
 #endif
