9adf60e1cb7ede4d88febc666c39c49621b04349
Aleksey Tulinov <aleksey_t@users.sourceforge.net>
2012-11-25 18:57:53 +0000

    +inreased read/write timeout to 15 secs
    git-svn-id: https://svn.code.sf.net/p/remotefs/code/trunk@1300 7764f161-3e89-4957-9ae6-8e1f4830ae4a

--- src/config.h
+++ src/config.h
@@ -45,8 +45,8 @@ struct list;
 #define MAX_LISTEN_ADDRESSES    16
 #define LISTEN_BACKLOG          MAX_LISTEN_ADDRESSES
 #define ALL_ACCESS_USERNAME     "*"
-#define DEFAULT_RECV_TIMEOUT	1000000     /* usecs */
-#define DEFAULT_SEND_TIMEOUT	1000000     /* usecs */
+#define DEFAULT_RECV_TIMEOUT	15 * 1000000     /* usecs */
+#define DEFAULT_SEND_TIMEOUT	15 * 1000000     /* usecs */
 #define DEFAULT_CONNECT_TIMEOUT	1000000     /* usecs */
 
 #define STAT_BLOCK_SIZE sizeof(uint32_t) /* mode */ \
